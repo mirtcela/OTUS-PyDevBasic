8c5a5e31ee0d61d1c1c3ff5ed5468d99107ebcb7
Anastasiya <mirtcela@yandex.ru>
2021-03-20 17:05:28 +0300

    - создание функций power_numbers и filter_numbers

--- homework_01/main.py
+++ homework_01/main.py
@@ -2,15 +2,15 @@
 Домашнее задание №1
 Функции и структуры данных
 """
+import math
 
 
-def power_numbers():
+def power_numbers(*number):
     """
-    функция, которая принимает N целых чисел,
-    и возвращает список квадратов этих чисел
-    >>> power_numbers(1, 2, 5, 7)
-    <<< [1, 4, 25, 49]
-    """
+       функция, которая принимает N целых чисел,
+       и возвращает список квадратов этих чисел
+       """
+    return [number ** 2 for number in number]
 
 
 # filter types
@@ -19,14 +19,31 @@ EVEN = "even"
 PRIME = "prime"
 
 
-def filter_numbers():
+def is_prime(args):
+    """
+    функция, которая принимает целое число,
+        и проверяет является ли число простым
+    """
+    if args <= 1:
+        return False
+    if args <= 3:
+        return True
+    for i in range(2, args-1):
+        if (args % i) == 0:
+            return False
+    return True
+
+
+def filter_numbers(args, type):
+    if type == "odd":
+        return list(filter(lambda x: x % 2 == 1, args))
+    if type == "even":
+        return list(filter(lambda x: x % 2 == 0, args))
+    if type == "prime":
+        return list(filter(lambda x: is_prime(x), args))
+
     """
     функция, которая на вход принимает список из целых чисел,
     и возвращает только чётные/нечётные/простые числа
     (выбор производится передачей дополнительного аргумента)
-
-    >>> filter_numbers([1, 2, 3], ODD)
-    <<< [1, 3]
-    >>> filter_numbers([2, 3, 4, 5], EVEN)
-    <<< [2, 4]
     """
